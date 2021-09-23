cfced9dcb25fa3f45ee6e59f049323f8c0032e43
abdlaziss <abdhulazis207@gmail.com>
2021-09-23 20:21:28 +0700

    penambahan isi application

---
+++ application/controllers/Contoh1.php
@@ -0,0 +1,9 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+    public function index()
+    {
+        echo "<h1>Perkenalkan<h1/>";
+        echo "Nama Saya Abdul Azis saya Tinggal di Jakarta Barat, olahraga yang saya sukai adalah bermain Bulutangkis";
+    }
+}
